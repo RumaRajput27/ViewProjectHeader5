2d5ed53c09b236df2cdf77575c158da604764821
RumaRajput27 <rumarajput27@gmail.com>
2024-10-07 23:11:45 +0530

    updated

--- controllers/successController.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-// GET handler to serve the success page
-exports.getSuccessPage = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views', 'success.html'));
-};
